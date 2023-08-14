deca9c4628934397bca906273278d7c766046a1a
Alexander Malukhin <keeonenew@yandex.ru>
2023-08-14 17:17:00 +0300

    feat: add MoviesChema

---
+++ module/movies.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+// const bcrypt = require('bcryptjs');
+
+const MoviesChema = new mongoose.Schema({
+  country: {
+    type: String,
+  },
+
+  director: {
+    type: String,
+  },
+
+  duration: {
+    type: Number,
+  },
+
+  year: {
+    type: String,
+  },
+
+  description: {
+    type: String,
+  },
+
+  image: {
+    type: String,
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Не корректный URL',
+    },
+  },
+
+  trailerLink: {
+    type: String,
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Не корректный URL',
+    },
+  },
+
+  thumbnail: {
+    type: String,
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Не корректный URL',
+    },
+  },
+
+  owner: {
+    type: String,
+    required: true,
+  },
+
+  movieId: {
+    type: Number,
+    required: true,
+  },
+
+  nameRU: {
+    type: String,
+  },
+
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movies', MoviesChema);
