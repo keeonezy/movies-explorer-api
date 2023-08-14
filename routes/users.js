48c974bdca692d997919d49e5ea119e1585edfad
Alexander Malukhin <keeonenew@yandex.ru>
2023-08-14 17:31:04 +0300

    feat: add routes user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { getUserInfo, updateUser } = require('../controllers/users');
+const { getUserInfo, updateUser, createUser, loginUser } = require('../controllers/users');
 
 router.get('/users/me', getUserInfo);
 router.patch('/users/me', updateUser);
+router.patch('/signup', createUser);
+router.patch('/signin', loginUser);
 
 module.exports = router;
