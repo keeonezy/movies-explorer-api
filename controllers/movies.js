2ceb6b4c04cd2f152e9c15796eb919f601ce4a62
Alexander Malukhin <keeonenew@yandex.ru>
2023-08-15 23:33:45 +0300

    feat: add controllers movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -7,8 +7,40 @@ const BadRequestError = require('../errors/status-400');
 const ForbiddenError = require('../errors/status-403');
 const NotFoundError = require('../errors/status-404');
 
-module.exports.getMovies = (req, res, next) => { };
+module.exports.getMovies = (req, res, next) => {
+  Movie
+    .find({}).sort({ createdAt: -1 })
+    .then((cards) => res.send(cards))
+    .catch(next);
+};
 
-module.exports.createMovies = (req, res, next) => { };
+module.exports.createMovies = (req, res, next) => {
+  const body = { ...req.body, owner: req.user._id };
 
-module.exports.deleteMovies = (req, res, next) => { };
+  Movie.create(body)
+    // 201 статус должен быть успешным
+    .then((card) => res.status(STATUS_CREATED).send(card))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        next(new BadRequestError('Не правильно переданы данные'));
+      } else next(err);
+    });
+};
+
+module.exports.deleteMovies = (req, res, next) => {
+  Movie.findByIdAndDelete(req.params.cardId)
+    .orFail(() => { throw new NotFoundError('Карточка для удаления не найдена'); })
+    .then((card) => card.owner.equals(req.user._id))
+    .then((match) => {
+      if (!match) {
+        throw new ForbiddenError('Нет прав для удаления данной карточки');
+      }
+      return Movie.findByIdAndRemove(req.params.cardId);
+    })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        next(new BadRequestError('Не правильно переданы данные'));
+      } else next(err);
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 // Роутер пользователя
 const userRouter = require('./users');
-// const moviesRouter = require('./movies');
+const moviesRouter = require('./movies');
 const { createUser, loginUser } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { signupValidator, signinValidator } = require('../middlewares/validation');
@@ -12,7 +12,7 @@ router.post('/signup', signupValidator, createUser);
 router.use(auth);
 
 router.use(userRouter);
-// router.use(moviesRouter);
+router.use(moviesRouter);
 
 router.use('/*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
