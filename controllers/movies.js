a104d3fa1cb3feaa57d4cd1c5e501af31047d49b
Alexander Malukhin <keeonenew@yandex.ru>
2023-08-16 01:21:46 +0300

    fix: controller movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -8,7 +8,7 @@ const NotFoundError = require('../errors/status-404');
 module.exports.getMovies = (req, res, next) => {
   Movie
     .find({}).sort({ createdAt: -1 })
-    .then((cards) => res.send(cards))
+    .then((movies) => res.send(movies))
     .catch(next);
 };
 
@@ -17,7 +17,7 @@ module.exports.createMovies = (req, res, next) => {
 
   Movie.create(body)
     // 201 статус должен быть успешным
-    .then((card) => res.status(STATUS_CREATED).send(card))
+    .then((movie) => res.status(STATUS_CREATED).send(movie))
     .catch((err) => {
       if (err instanceof mongoose.Error.ValidationError) {
         next(new BadRequestError('Не правильно переданы данные'));
@@ -28,14 +28,14 @@ module.exports.createMovies = (req, res, next) => {
 module.exports.deleteMovies = (req, res, next) => {
   Movie.findByIdAndDelete(req.params.movieId)
     .orFail(() => { throw new NotFoundError('Карточка для удаления не найдена'); })
-    .then((card) => card.owner.equals(req.user._id))
+    .then((movie) => movie.owner.equals(req.user._id))
     .then((match) => {
       if (!match) {
         throw new ForbiddenError('Нет прав для удаления данной карточки');
       }
       return Movie.findByIdAndRemove(req.params.movieId);
     })
-    .then((card) => res.send({ data: card }))
+    .then((movie) => res.send({ data: movie }))
     .catch((err) => {
       if (err instanceof mongoose.Error.CastError) {
         next(new BadRequestError('Не правильно переданы данные'));
